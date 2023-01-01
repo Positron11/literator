5b9d06abaa73619933fd8184535dbb46e20ad1d3
Positron11 <aarushbeleren11@gmail.com>
2023-01-01 21:41:52 +0530

    ADD SCROLL INDICATOR

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,16 +1,27 @@
 // elements
 const navbar = document.getElementById("navbar")
 const sidebar = document.getElementById("sidebar")
+const sidebarContent = document.getElementById("sidebar_content")
 
 // initialization
 resizeSidebar();
+sidebarScrollIndicatorDisplay();
 
 // navbar sticky control
 const stickyObserver = new IntersectionObserver(([e]) => e.target.classList.toggle('is-sticky', e.intersectionRatio < 1), {threshold: [1]});
 stickyObserver.observe(navbar)
 
 // event listeners
-window.addEventListener("scroll", () => {resizeSidebar();});
+window.addEventListener("scroll", () => {
+	resizeSidebar();
+	sidebarScrollIndicatorDisplay();
+});
+sidebarContent.addEventListener("scroll", () => {
+	sidebarScrollIndicatorDisplay();
+});
 
 // navbar height
-function resizeSidebar() {sidebar.style.maxHeight = String(window.innerHeight - (sidebar.getBoundingClientRect().top + 160)) + "px";}
+function resizeSidebar() {sidebar.style.maxHeight = String(window.innerHeight - (sidebar.getBoundingClientRect().top + 60)) + "px";}
+
+// sidebar scroll indicator
+function sidebarScrollIndicatorDisplay() {sidebarContent.classList.toggle("fully-scrolled", (sidebarContent.scrollHeight - sidebarContent.scrollTop) < sidebarContent.clientHeight + 5);}
