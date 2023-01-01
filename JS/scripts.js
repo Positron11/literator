0990ba520efd9011a41e07dcf2f39035865dbfed
Positron11 <aarushbeleren11@gmail.com>
2023-01-01 19:00:06 +0530

    DYNAMIC HEIGHT SIDEBAR ALSO MAKE BLACK

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,20 @@
+// elements
 const navbar = document.getElementById("navbar")
+const sidebar = document.getElementById("sidebar")
+
+// initialization
+resizeSidebar();
+
+// navbar sticky control
 const stickyObserver = new IntersectionObserver(([e]) => e.target.classList.toggle('is-sticky', e.intersectionRatio < 1), {threshold: [1]});
 stickyObserver.observe(navbar)
+
+// event listeners
+window.addEventListener("scroll", () => {
+	resizeSidebar();
+});
+
+// navbar height function
+function resizeSidebar() {
+	sidebar.style.maxHeight = String(window.innerHeight - (sidebar.getBoundingClientRect().top + 160)) + "px";
+}
