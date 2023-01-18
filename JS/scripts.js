86c1681b787f10a5228b891dab2692c39a4d664a
Positron11 <aarushbeleren11@gmail.com>
2023-01-19 01:15:05 +0530

    ADD SUPER COOL DYNAMICALLY CALCULATED BACKGROUNDS

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,11 +3,28 @@ const navbar = document.getElementById("navbar");
 const main = document.getElementById("main");
 const bulletin = document.getElementById("bulletin");
 const bulletinContent = document.getElementById("bulletin_content");
+const storyPreviewBackgrounds = document.querySelectorAll("#story_previews .story .background-image");
 
 
 // initialization
 resizeBulletin();
 bulletinScrollStatus();
+
+// get story preview background image brightness
+Array.from(storyPreviewBackgrounds).forEach(background => {
+	path = window.getComputedStyle(background, false).backgroundImage.slice(4, -1).replace(/"/g, "");
+	getBrightness(path, (b) => {
+		// define scales
+		x0 = 0, x1 = 255;
+		y0 = 0.1, y1 = 0.3;
+
+		// map linear scale to ogarithmic scale
+		y = Math.exp(((b-x0)/(x1-x0)) * (Math.log(y1) - Math.log(y0)) + Math.log(y0));
+		
+		// set opacity
+		background.style.opacity = y;
+	});
+});
 
 
 // navbar sticky control
@@ -49,3 +66,37 @@ function toggleNavbarMenu() {
 function bulletinScrollStatus() {
 	bulletinContent.classList.toggle("fully-scrolled", (bulletinContent.scrollHeight - bulletinContent.scrollTop) < bulletinContent.clientHeight + 10);
 }
+
+
+// get image brightness
+function getBrightness(imageSrc, callback) {
+	const img = document.createElement("img");
+	img.src = imageSrc;
+	img.crossOrigin = 'anonymous';
+	img.style.display = "none";
+	document.body.appendChild(img);
+	let colorSum = 0;
+
+	img.onload = function() {
+		const canvas = document.createElement("canvas");
+		canvas.width = this.width;
+		canvas.height = this.height;
+		const ctx = canvas.getContext("2d");
+		ctx.drawImage(this, 0, 0);
+  
+		const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		const data = imageData.data;
+		let r, g, b, avg;
+  
+		for(let x=0, len=data.length; x<len; x+=4) {
+			r = data[x];
+			g = data[x+1];
+			b = data[x+2];
+			avg = Math.floor((r+g+b) / 3);
+			colorSum += avg;
+		}
+  
+		const brightness = Math.floor(colorSum / (this.width * this.height));
+		callback(brightness);
+	}
+};
